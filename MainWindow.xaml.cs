d5a4d2a4b983fd3bbdad8e6ebe04b52f1a3ff517
Naoufel <Naoufel@DESKTOP-40OKKQE>
2023-10-18 15:31:44 +0200

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AMIAR_TP1_EX5
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        // Fonction pour inverser une chaîne de caractères
+        private string ReverseString(string input)
+        {
+            char[] Tableau = input.ToCharArray();
+            Array.Reverse(Tableau);
+            return new string(Tableau);
+        }
+
+
+        private void BT_CALCUL_Click(object sender, RoutedEventArgs e)
+        {
+            string entree = TB_ENTREE.Text;
+            string reversed = ReverseString(entree);
+            TB_DISPLAY.Text = reversed;
+        }
+    }
+}
